9fbf6481b67598a13832eac14099d39924939ddb
ndthanh143 <nguyenduythanh421.tna@Gmail.com>
2022-12-24 21:33:51 +0700

    change url

---
+++ src/api/axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const BASE_URL ='https://api-sport-clothes.onrender.com/api/v1/'
+
+console.log(process.env.NODE_ENV);
+export default axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+});
+
+// export const axiosPrivate = axios.create({
+//     baseURL: BASE_URL,
+//     headers: { 'Content-Type': 'application/json' },
+//     withCredentials: true,
+// });
